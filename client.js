24cbf7ff38ce0a4e2a351be2e6b1ec8d92476006
Izaias Henrique <izaias.rique@gmail.com>
2023-01-28 19:10:06 -0300

    feat: add client instance to use discord.js

---
+++ client.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+module.exports = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+ });
